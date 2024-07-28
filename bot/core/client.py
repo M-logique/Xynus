ef971b3956400929ca8e7f54a5c09e94acca0d50
M-logique <lmy615730@gmail.com>
2024-07-28 11:34:18 +0330

    Changed status name

--- bot/core/client.py
+++ bot/core/client.py
@@ -43,7 +43,7 @@ class Client(_commands.Bot):
     async def on_ready(self):
 
         
-        await self.change_presence(activity=_Activity(type=_ActivityType.streaming, url="https://twitch.tv/discord", name="?help - TTK 2"))
+        await self.change_presence(activity=_Activity(type=_ActivityType.streaming, url="https://twitch.tv/discord", name="TTK 2 - ?help"))
         if self.cogs != {}: return self.logger.warn("Skipped loading cogs: Reconnecting")
 
         self.logger.success(f"Discord Client Logged in as {self.user.name}")

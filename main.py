2af5c1ea77d430bc947b2e0246cf419e189cdfcd
M-logique <lmy615730@gmail.com>
2024-07-28 11:18:42 +0330

    removed proxy parameter from main.py

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ from bot.core.client import Client
 
 client = Client(
                 intents=discord.Intents.all(),
-                allowed_mentions=discord.AllowedMentions(replied_user=False),
-                proxy="http://127.0.0.1:2334"
+                allowed_mentions=discord.AllowedMentions(replied_user=False)
                 )
 
 

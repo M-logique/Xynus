a13bdf3ce8e7dd5861fc51cde4468246bc901aef
M-logique <lmy615730@gmail.com>
2024-08-09 12:48:53 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-import discord
+from discord import AllowedMentions, Intents
 
 from bot.core.client import Client
 
 client = Client(
-                intents=discord.Intents.all(),
-                allowed_mentions=discord.AllowedMentions(replied_user=False)
-                )
+    intents=Intents.all(),
+    allowed_mentions=AllowedMentions(
+        replied_user=False
+    )
+)
 
 
 client.run()

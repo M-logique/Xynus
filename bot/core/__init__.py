c8ac1e58e01fa145198dd59f1bf5345585597ac2
M-logique <lmy615730@gmail.com>
2024-08-22 14:47:19 +0330

    removed color from bot.utils.__init__

--- bot/core/__init__.py
+++ bot/core/__init__.py
@@ -1,9 +1,8 @@
 from discord import Color as _Color
 from discord import Object as _Object
 
-from .client import Client
+from .client import Xynus
 from .logger import Logger
 from .settings import settings as _settings
 
-color = _Color.from_rgb(*_settings.MAIN_COLOR)
 

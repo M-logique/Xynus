4a62d9e6da045662529fc21096060e97e09aaca2
M-logique <lmy615730@gmail.com>
2024-08-22 14:41:47 +0330

    Updated base_cog, changed its name to XynusCog

--- bot/templates/cogs.py
+++ bot/templates/cogs.py
@@ -1,10 +1,15 @@
 from discord.ext import commands as _commands
 
-from ..core import Client
+from ..core import Xynus as _Xynus
+from typing import Optional
+from discord import PartialEmoji
 
+class XynusCog(_commands.Cog):
 
-class Cog(_commands.Cog):
-
-    def __init__(self, client: Client) -> None:
-
+    def __init__(self, client: _Xynus) -> None:
         self.client = client
+        self.emoji = Optional[PartialEmoji] = None 
+        
+        # Since I'm organizing commands based on Cogs,
+        # I need an emoji for the SelectOption in the HelpSelect,
+        # which will be defined here.
